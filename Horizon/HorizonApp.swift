f319b04188db0adcdd4321d7dd698b6c3dc857b1
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2024-03-12 22:23:03 +0300

    Initial Commit

---
+++ Horizon/HorizonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizonApp.swift
+//  Horizon
+//
+//  Created by Tariq Almazyad on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HorizonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
